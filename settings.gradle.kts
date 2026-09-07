a440c1a1828a24de3a84addfcbebe57634723812
Anshul Badoni <91309365+AnshulBadoni@users.noreply.github.com>
2026-09-07 19:20:39 +0530

    Fix formatting issues in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@ rootProject.name = "CloudstreamPlugins"
 
 val disabled = listOf<String>("OpJav")
 
+
 File(rootDir, ".").eachDir { dir ->
     if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
         include(dir.name)
